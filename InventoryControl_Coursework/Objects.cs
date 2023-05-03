e1d8d24308e546ca89213bea86db1cb6ba05527c
Vorpark <nik.vorpark@mail.ru>
2023-05-03 16:46:08 +0300

    Повтор

--- InventoryControl_Coursework/Objects.cs
+++ InventoryControl_Coursework/Objects.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace InventoryControl_Coursework
 {
-    internal class Objects
+    public class Materials // Все определeнные материалы
     {
+        public string Name { get; set; }
+        public List<Material> Material {get; set;}
+    }
+    public class Material // Определенный материал закрепленный за определенной ячейкой
+    {
+        public string Name { get; set; }
+        public Shelving Shelving { get; set; }
+        public ShelvingUnit ShelvingUnit { get; set; }
+        public int Count { get; set; }
+    }
+    public class Shelving // Стеллаж
+    {
+        public int Number { get; set; }
+        public List<ShelvingUnit> ShelvingUnits { get; set; }
+    }
+    public class ShelvingUnit //Ячейка стеллажа
+    {
+        public int Number { get; set; }
     }
 }
