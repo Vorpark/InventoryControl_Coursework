3d4f1ab380173f4ae20205282e1382ceb0fa40f3
Vorpark <nik.vorpark@mail.ru>
2023-05-04 14:45:23 +0300

    Fix проверок

--- InventoryControl_Coursework/AddObject.Designer.cs
+++ InventoryControl_Coursework/AddObject.Designer.cs
@@ -43,21 +43,21 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(114, 6);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.Size = new System.Drawing.Size(177, 20);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(114, 32);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(128, 20);
+            this.textBox2.Size = new System.Drawing.Size(177, 20);
             this.textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
             this.textBox3.Location = new System.Drawing.Point(114, 58);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(128, 20);
+            this.textBox3.Size = new System.Drawing.Size(177, 20);
             this.textBox3.TabIndex = 2;
             // 
             // label1
@@ -100,7 +100,7 @@
             // 
             this.textBox4.Location = new System.Drawing.Point(114, 84);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(128, 20);
+            this.textBox4.Size = new System.Drawing.Size(177, 20);
             this.textBox4.TabIndex = 7;
             // 
             // button1
@@ -127,7 +127,7 @@
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Name = "AddNewObject";
+            this.Name = "AddObject";
             this.Text = "AddNewObject";
             this.ResumeLayout(false);
             this.PerformLayout();
--- InventoryControl_Coursework/AddObject.cs
+++ InventoryControl_Coursework/AddObject.cs
@@ -20,17 +20,17 @@ namespace InventoryControl_Coursework
                 MessageBox.Show("Поле название материала не может быть пустым!", "Ошибка!", MessageBoxButtons.OK);
                 check = false;
             }
-            if (int.TryParse(textBox2.Text, out int count) && textBox2.Text == "")
+            if (!int.TryParse(textBox2.Text, out int count) && textBox2.Text == "")
             {
                 MessageBox.Show("Поле количество материала введено неверно!", "Ошибка!", MessageBoxButtons.OK);
                 check = false;
             }
-            if (int.TryParse(textBox3.Text, out int numberShelving) && textBox3.Text == "")
+            if (!int.TryParse(textBox3.Text, out int numberShelving) && textBox3.Text == "")
             {
                 MessageBox.Show("Поле номер стеллажа введено неверно!", "Ошибка!", MessageBoxButtons.OK);
                 check = false;
             }
-            if (int.TryParse(textBox4.Text, out int numberShelvingUnit) && textBox4.Text == "")
+            if (!int.TryParse(textBox4.Text, out int numberShelvingUnit) && textBox4.Text == "")
             {
                 MessageBox.Show("Поле номер ячейки введено неверно!", "Ошибка!", MessageBoxButtons.OK);
                 check = false;
@@ -56,10 +56,10 @@ namespace InventoryControl_Coursework
                 if (check)
                 {
                     Material material = new Material() { Name = textBox1.Text, Count = count };
-                    Shelving shelving = new Shelving() { Number = numberShelving }; // Проверка на уникальность стеллажа
+                    Shelving shelving = new Shelving() { Number = numberShelving }; 
                     ShelvingUnit shelvingUnit = new ShelvingUnit() { Number = numberShelvingUnit };
                     shelvingUnit.Material = material;
-                    shelving.ShelvingUnits.Add(shelvingUnit); //Проверка на уникальность ячейки и занятость
+                    shelving.ShelvingUnits.Add(shelvingUnit);
 
                     string text = $"{shelving.Number} {shelvingUnit.Number} {material.Name} {material.Count}";
                     using (StreamWriter writer = new StreamWriter(path, true))
